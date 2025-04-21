04df54f98aef017a7ddf80ae6c9b1a2897df375d
Kakou2106 <perrinfranck13003@gmail.com>
2025-04-21 14:03:33 +0200

    fix: dynamique du port pour Render

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
 import os
-port = int(os.environ.get("PORT", 10000))
+
+port = int(os.environ.get("PORT", 10000))  # Utilise le port dynamique de Render, sinon 10000 par défaut.
 app.run(host="0.0.0.0", port=port)
